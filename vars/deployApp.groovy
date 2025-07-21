d35786d983f6e047998261299094e2abbf6b4e9f
Umair1012 <139431715+Umair1012@users.noreply.github.com>
2025-07-21 18:23:50 +0500

    Update deployApp.groovy

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,4 +1,4 @@
-def call(String composeFile) {
+def call(String composeFile, String branch) {
     sh """
         docker-compose -f ${composeFile} --env-file .env down
         docker-compose -f ${composeFile} --env-file .env pull
